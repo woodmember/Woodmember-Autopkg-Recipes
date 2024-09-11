c4a68e14eb70115b3b99793ad1c155ea5d6f78d8
woodmember <82789199+woodmember@users.noreply.github.com>
2024-09-11 10:58:10 +1000

    Update FalconDownloader.py

--- Crowdstrike/FalconDownloader.py
+++ Crowdstrike/FalconDownloader.py
@@ -112,8 +112,13 @@ class FalconDownloader(Processor):
         sensor_name = sensor_data["resources"][0]["name"]
         sensor_sha = sensor_data["resources"][0]["sha256"]
 
+        # Create downloads folder in the recipe directory
+        downloads_dir = os.path.join(self.env["RECIPE_DIR"], "downloads")
+        if not os.path.exists(downloads_dir):
+            os.makedirs(downloads_dir)
+
         # Download the sensor
-        download_path = os.path.join(self.env["RECIPE_CACHE_DIR"], sensor_name)
+        download_path = os.path.join(downloads_dir, sensor_name)
         download_cmd = self.curl_cmd(f"{sensor_dl}?id={sensor_sha}", headers={"Authorization": f"Bearer {bearer}"}, output=download_path)
         _, download_stderr = self.run_curl(download_cmd)
 
